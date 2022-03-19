30755365308fc388f711acd460decbe366208a89
Eder Narvaez <edern43@gmail.com>
2022-03-18 23:19:45 -0300

    first commit8

--- moviesgood/settings.py
+++ moviesgood/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4w#k0qv^56p5(a5*r=4os0gi+1hvi%uzc0#!jsa2!+!ps*k!!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.4']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
